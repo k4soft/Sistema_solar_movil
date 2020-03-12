c45aafa13a40af03eb900377c2c06cb7fd59f2b1
diegogarcia <consultor@dgpragmatic.com>
2020-03-11 19:15:40 -0500

    se corrige título

--- app/src/main/java/co/com/k4soft/sistemasolar/MainActivity.java
+++ app/src/main/java/co/com/k4soft/sistemasolar/MainActivity.java
@@ -52,31 +52,13 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
-        /*
-         txtEmpleado.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
-                busquedaCalidadMuestroAdapter.getFilter().filter(charSequence);
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-
-            }
-        });
-         */
     }
 
     private void loadInfo() {
         List<Planeta> listaPlanetas = new ArrayList<>();
         listaPlanetas.add(new Planeta(R.drawable.tierra, "Tierra", "La Tierra (del latín Terra,17\u200B deidad romana equivalente a Gea, diosa griega de la feminidad y la fecundidad) es un planeta del sistema solar que gira alrededor de su estrella —el Sol— en la tercera órbita más interna"));
         listaPlanetas.add(new Planeta(R.drawable.marte, "Marte", "Marte es el cuarto planeta en orden de distancia al Sol y el segundo más pequeño del sistema solar, después de Mercurio."));
-        listaPlanetas.add(new Planeta(R.drawable.jupiter, "Marte", "Júpiter es el quinto planeta del sistema solar. Forma parte de los denominados planetas exteriores o gaseosos. Recibe su nombre del dios romano Júpiter (Zeus en la mitología griega)."));
+        listaPlanetas.add(new Planeta(R.drawable.jupiter, "Júpiter", "Júpiter es el quinto planeta del sistema solar. Forma parte de los denominados planetas exteriores o gaseosos. Recibe su nombre del dios romano Júpiter (Zeus en la mitología griega)."));
         planetaAdapter = new PlanetaAdapter(this, listaPlanetas);
         listViewPlanetas.setAdapter(planetaAdapter);
     }
